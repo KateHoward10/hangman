220932e068d219a7ae44950d8947b6769aa9bfab
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-18 00:01:05 +0100

    Display lives

--- public/hangman.js
+++ public/hangman.js
@@ -1,11 +1,13 @@
 var socket = io.connect('http://localhost:4000');
 
 let currentChallenge = '';
+let lives = 10;
 
 const challengeInput = document.getElementById('challenge');
 const form = document.getElementById('challenge_form');
 const display = document.querySelector('.display-area');
 const letters = document.getElementById('letters_container');
+const livesContainer = document.getElementById('lives_container');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -16,6 +18,7 @@ form.addEventListener('submit', e => {
 socket.on('challenge', data => {
   currentChallenge = data;
   display.textContent = data.replace(/[a-zA-Z]/gi, '?');
+  livesContainer.textContent = 'Lives: ' + Array(lives).fill('|').join('');
   for (let i = 0; i < 26; i++) {
     const letter = String.fromCharCode(i + 65);
     const letterButton = document.createElement('button');
@@ -26,6 +29,9 @@ socket.on('challenge', data => {
       const guess = e.target.value;
       if (currentChallenge.indexOf(guess) > -1) {
         display.textContent = display.textContent.split('').map((char, i) => currentChallenge[i] === guess ? guess : char).join('');
+      } else {
+        lives--;
+        livesContainer.textContent = 'Lives: ' + Array(lives).fill('|').join('');
       };
     })
     letters.appendChild(letterButton);
