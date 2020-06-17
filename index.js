72d586412fc4438291e11113e10e1f6ccf9e8d66
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-17 23:01:27 +0100

    Emit challenge event

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ app.use(express.static('public'));
 var io = socket(server);
 
 io.on('connection', function(socket) {
-  console.log("Socket connected");
+  socket.on('challenge', data => {
+    io.emit('challenge', data);
+  })
 });
--- public/hangman.js
+++ public/hangman.js
@@ -1,1 +1,15 @@
 var socket = io.connect('http://localhost:4000');
+
+const challengeInput = document.getElementById('challenge');
+const form = document.getElementById('challenge_form');
+const display = document.querySelector('.display-area');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  socket.emit('challenge', challengeInput.value);
+  challengeInput.value = '';
+});
+
+socket.on('challenge', data => {
+  display.textContent = data.replace(/[a-zA-Z]/gi, '?');
+});
