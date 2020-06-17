888254c953d335f1f939334bc2db893d26836fd7
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-17 23:24:07 +0100

    Add letter buttons

--- public/hangman.js
+++ public/hangman.js
@@ -3,6 +3,7 @@ var socket = io.connect('http://localhost:4000');
 const challengeInput = document.getElementById('challenge');
 const form = document.getElementById('challenge_form');
 const display = document.querySelector('.display-area');
+const letters = document.getElementById('letters_container');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -12,4 +13,11 @@ form.addEventListener('submit', e => {
 
 socket.on('challenge', data => {
   display.textContent = data.replace(/[a-zA-Z]/gi, '?');
+  for (let i = 0; i < 26; i++) {
+    const letter = String.fromCharCode(i + 97);
+    const letterButton = document.createElement('button');
+    letterButton.value = letter;
+    letterButton.textContent = letter;
+    letters.appendChild(letterButton);
+  }
 });
