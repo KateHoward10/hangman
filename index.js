da2cba5292bc273161c2db998f911de7054f48ab
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-17 22:34:55 +0100

    Add connection between server and client

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 
 var app = express();
 var server = app.listen(4000, function () {
@@ -6,3 +7,9 @@ var server = app.listen(4000, function () {
 });
 
 app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', function(socket) {
+  console.log("Socket connected");
+});
