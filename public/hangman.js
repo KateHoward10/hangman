463f6abfc6e82e76527ea317ea02c7119c37eb59
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-18 15:28:12 +0100

    Use Vue

--- public/hangman.js
+++ public/hangman.js
@@ -9,40 +9,44 @@ socket.on('connect', () => {
   socket.emit('join', room);
 });
 
-let currentChallenge = '';
-let lives = 10;
-
-const challengeInput = document.getElementById('challenge');
-const form = document.getElementById('challenge_form');
-const display = document.querySelector('.display-area');
-const letters = document.getElementById('letters_container');
-const livesContainer = document.getElementById('lives_container');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  socket.emit('challenge', challengeInput.value.toUpperCase());
-  challengeInput.value = '';
-});
-
-socket.on('challenge', data => {
-  currentChallenge = data;
-  display.textContent = data.replace(/[a-zA-Z]/gi, '?');
-  livesContainer.textContent = 'Lives: ' + Array(lives).fill('|').join('');
-  for (let i = 0; i < 26; i++) {
-    const letter = String.fromCharCode(i + 65);
-    const letterButton = document.createElement('button');
-    letterButton.value = letter;
-    letterButton.textContent = letter;
-    letterButton.classList.add('letter-button');
-    letterButton.addEventListener('click', e => {
-      const guess = e.target.value;
-      if (currentChallenge.indexOf(guess) > -1) {
-        display.textContent = display.textContent.split('').map((char, i) => currentChallenge[i] === guess ? guess : char).join('');
-      } else {
-        lives--;
-        livesContainer.textContent = 'Lives: ' + Array(lives).fill('|').join('');
-      };
-    })
-    letters.appendChild(letterButton);
+const hangman = new Vue({
+  el: '#hangman',
+  data: {
+    isChallenging: false,
+    currentChallenge: '',
+    lives: 10,
+    guesses: [],
+    alphabet: Array.from(Array(26).keys(), i => String.fromCharCode(i + 65))
+  },
+  computed: {
+    formattedChallenge: function() {
+      return this.currentChallenge.split('').map((char, i) => {
+        if (char === ' ') {
+          return ' ';
+        } else if (this.guesses.some(guess => this.currentChallenge[i] === guess)) {
+          return char;
+        } else return '?';
+      }).join('');
+    },
+    livesTally: function() {
+      return Array(this.lives).fill('|').join('');
+    }
+  },
+  created() {
+    socket.on('challenge', challenge => {
+      this.currentChallenge = challenge;
+    });
+  },
+  methods: {
+    submitChallenge: function(e) {
+      e.preventDefault();
+      const challengeInput = document.getElementById('challenge');
+      socket.emit('challenge', challengeInput.value.toUpperCase());
+      challengeInput.value = '';
+    },
+    guessLetter: function(guess) {
+      this.guesses.push(guess);
+      if (this.currentChallenge.indexOf(guess) === -1) this.lives--;
+    }
   }
 });
